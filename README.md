a62b9025ef0bb583a678f40b0efea91a03efbf0b
tvrcgo <tvrcgo@gmail.com>
2016-01-19 02:10:37 +0800

    bee.

--- README.md
+++ README.md
@@ -1,2 +1,79 @@
 # Bee
-勤劳的小蜜蜂
+变态的小蜜蜂
+
+## Install
+```
+npm i tvrcgo/bee
+```
+
+## Usage
+```js
+var Bee = require('bee');
+
+var bee = Bee({
+    bid: 'i-xx',
+    author: 'username',
+    tag: 'i-xx-news'
+});
+
+// in worker
+data(function(data){
+    // Store data.
+    bee.store(data);
+
+    // ...
+```
+
+### honey
+```js
+// Assign single.
+bee.honey('title', function(data){
+    return 'hello bee!';
+})
+
+// Assign many.
+bee.honey({
+    title: data.mainTitle,
+    originalUrl: data.url,
+    category: function(data){
+        // process data.
+        return data;
+    }
+})
+```
+
+### honey extend
+```js
+// Feed honey extend.
+bee.extend(url, {
+    xtype: 'tags',
+    list: 'headlines'
+})
+```
+
+### flower
+```js
+// Feed flower.
+bee.flower(url);
+// Feed flower with data.
+bee.flower(url, {
+    topic: 'xxx'
+})
+// Feed flower with task.data .
+bee.flower(url, bee.xflower(task));
+// Feed flower with task.data and extends.
+bee.flower(url, bee.xflower(task, {
+    tags: 'xxx'
+}))
+```
+
+### done
+```js
+// in worker.
+.done(function(ctx){
+    bee.done(task);
+})
+```
+
+## License
+MIT
---
+++ index.js
@@ -0,0 +1,116 @@
+'use strict';
+
+/**
+ * 变态的小蜜蜂
+ * @param {object} opt
+ */
+function Bee(opt){
+    if (!(this instanceof Bee)) {
+        return new Bee(opt);
+    }
+    this.opt = opt || {};
+    this.flower = [];
+    this.extend = [];
+    this.honey = {};
+}
+
+/**
+ * 存数据
+ * @param data
+ * @param select
+ */
+Bee.prototype.store = function(data, select){
+    var keys = select || ['title', 'originalUrl', 'keywords', 'summary', 'coverPic'];
+    Object.keys(keys).forEach(function(key){
+        this.honey[key] = data[key] || '';
+    }.bind(this));
+    return this;
+}
+
+/**
+ * 扩展 honey
+ * @param  {[string, object]}   key
+ * @param  {function} fn
+ */
+Bee.prototype.honey = function(key, fn) {
+    if (key && typeof key === 'object') {
+        Object.keys(key).forEach(function(k){
+            var tar = key[k];
+            this.honey[k] = typeof tar === 'function' ? tar.call(this, this.honey[k]) : tar;
+        }.bind(this));
+        return this;
+    }
+    key = key || '_';
+    this.honey[key] = fn && fn.call(this, this.honey[key], function(data){
+        this.honey[key] = data;
+        return data;
+    }.bind(this));
+    return this;
+}
+
+/**
+ * 存 flower
+ * @param  {string} url
+ * @param  {object} data
+ */
+Bee.prototype.flower = function(url, data) {
+    this.flower.push({
+        url: url,
+        data: data;
+    });
+    return this;
+}
+
+/**
+ * 存 honey extend
+ * @param  {string} url
+ * @param  {object|array} data
+ */
+Bee.prototype.extend = function(url, data){
+    this.extend.push({
+        url: url,
+        data: [].concat(data);
+    })
+    return this;
+}
+
+/**
+ * 处理完成
+ * @param  {object} task
+ * @param  {object} ctx
+ */
+Bee.prototype.done = function(task, ctx){
+    task.harvest = mix(this.opt, {
+        // author, bid, tag
+        flower: this.flower,
+        honey: this.honey,
+        extend: this.extend
+    });
+    return task.done(null, task);
+}
+
+/**
+ * 解析 flower data
+ * @param  {object} task
+ * @param  {object} ext
+ */
+Bee.xflower = function(task, ext){
+    var json = {};
+    try {
+        json = JSON.parse(task.data);
+    } catch (e) {
+        // console.error(e);
+    }
+    if (ext && typeof ext === 'object') {
+        mix(json, ext);
+    }
+    return json;
+}
+
+/**
+ * 合并对象
+ * @return {object}
+ */
+function mix(){
+    // mix objects.
+}
